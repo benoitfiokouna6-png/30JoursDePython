4540347c4fbf584e04deceea4e3694b765ac77eb
benoitfiokouna6-png <benoitfiokouna6@gmail.com>
2025-07-28 00:05:58 +0000

    Day5

---
+++ Day5/Variable .py
@@ -0,0 +1,67 @@
+# déclaration
+
+list = () # ceci est une liste vide 
+
+list_marque_de_voiture = ["toyota", "ford", "bmw", "mercedes", "audit", "peugeot"]
+
+# la dure de ma liste 
+
+print(len(list_marque_de_voiture)) # la duré de  ma liste est (6)
+
+premier_élément = list_marque_de_voiture
+
+print(len(premier_élément))
+
+élément_moyen = list_marque_de_voiture
+
+print(len(list_marque_de_voiture
+))
+
+le_dernier_élément = list_marque_de_voiture
+
+print(len(list_marque_de_voiture
+))
+ 
+mixtes_data_types = ["FIOKOUNA","22","1.78","célibataire","agbalépédo"]
+
+# variables 
+
+IT_COMPANIES = ("facebook","google","microsoft","apple","ibm")
+
+print(len(IT_COMPANIES))
+
+print(IT_COMPANIES)
+
+print("facebook")
+
+print("microsoft")
+
+print("ibm")
+
+# insertion
+
+print(IT_COMPANIES.append("html"))
+
+IT_COMPANIES.append("html")
+
+ages : [19,22,19,24,20,25,26,24,25,24]
+
+# triés la liste 
+
+ages.sort()
+
+# retrouvés ages min et max
+
+ages_min = min(ages) # les ages min est 19
+print(ages_min)
+
+ages_max = max(ages) # les ages max est 26
+print(ages_max)
+
+# l'ages médian
+ages_median = ages[len(ages)] # l'ages médian est 24
+
+# la plages de l'ages moyen
+
+
+.
